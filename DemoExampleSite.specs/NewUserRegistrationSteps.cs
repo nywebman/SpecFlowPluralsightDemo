7fe7bddcd6777afdb008947c30487e239187675f
Brad Rosen <brosen@mdsol.com>
2014-10-21 11:53:23 -0400

    refactor to use WebBrowser.Current

--- DemoExampleSite.specs/NewUserRegistrationSteps.cs
+++ DemoExampleSite.specs/NewUserRegistrationSteps.cs
@@ -12,31 +12,24 @@ namespace DemoExampleSite.specs
         [Given]
         public void Given_I_m_on_the_registration_page()
         {
-            var ie = new IE("http://localhost:62988/Register.aspx");
-            ie.BringToFront();
-
-            ScenarioContext.Current.Add("browser", ie);
+            WebBrowser.Current.GoTo("http://localhost:62988/Register.aspx");
         }
         
         [When]
         public void When_I_enter_a_password_of_Pass()
         {
-            var ie = ScenarioContext.Current["browser"] as IE;
-            ie.TextField(Find.ById("Password")).TypeText("Pass");
-          //  ie.Eval("$('#Password').keypress()");//to get around a watin bug bcse the js keypress doesnt always fire
+            WebBrowser.Current.TextField(Find.ById("Password")).TypeText("Pass");
+            //  WebBrowser.Current.Eval("$('#Password').keypress()");//to get around a watin bug bcse the js keypress doesnt always fire
         }
         
         [Then]
         public void Then_the_password_strength_indicator_should_read_Poor()
         {
-            var ie = ScenarioContext.Current["browser"] as IE;
-
-            var strength = ie.Div(Find.ById("PasswordStrength")).InnerHtml;
+            var strength = WebBrowser.Current.Div(Find.ById("PasswordStrength")).InnerHtml;
 
             Assert.AreEqual("Poor", strength);
 
-            ie.AutoClose = false;
-            ie.Dispose();
+            WebBrowser.Current.Dispose();
         }
     }
 }
