c97c5882d21affa6586c1f2771c06b8112d9f520
Brad Rosen <brosen@mdsol.com>
2014-10-21 13:28:07 -0400

    abstract out control names into page class

---
+++ DemoExampleSite.specs/NavigationSteps.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace DemoExampleSite.specs
+{
+    [Binding]
+    public class NavigationSteps
+    {
+        [Given]
+        public void Given_I_m_on_the_registration_page()
+        {
+            //moved to a location for common steps
+            WebBrowser.Current.GoTo("http://localhost:62988/Register.aspx");
+        }
+    }
+}
---
+++ DemoExampleSite.specs/RegistrationPage.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatiN.Core;
+
+namespace DemoExampleSite.specs
+{
+    /*
+     to model the aspx/html/... page in case control names change
+     * so only need to reference the const here for controls, and only
+     * one place to change if the control name changes
+     */
+
+    //inherit from watin page class
+    internal class RegistrationPage : Page
+    {
+        private const string UserNameId = "UserName";
+        private const string EmailAddressId = "EmailAddress";
+        private const string PasswordId = "Password";
+        private const string ConfirmPasswordId = "ConfirmPassword";
+        private const string PasswordStrengthId = "PasswordStrength";
+        private const string ValidationErrorListId = "validationErrorList";
+        private const string DoRegisterId = "DoRegister";
+
+        public string UserName
+        {
+            get
+            {
+                return this.Document.TextField(Find.ById(UserNameId)).Text;
+            }
+            set
+            {
+                Document.TextField(Find.ById(UserNameId)).TypeText(value);
+            }
+        }
+        public string EmailAddress
+        {
+            get
+            {
+                return this.Document.TextField(Find.ById(EmailAddressId)).Text;
+            }
+            set
+            {
+                Document.TextField(Find.ById(EmailAddressId)).TypeText(value);
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.Document.TextField(Find.ById(PasswordId)).Text;
+            }
+            set
+            {
+                Document.TextField(Find.ById(PasswordId)).TypeText(value);
+                //WatiN typeText - the js keypress event doesnt fire
+                //WebBrowser.Current.Eval(string.Format("$('#{0}').keyPress()",Password));
+            }
+        }
+        public string ConfirmPassword
+        {
+            get
+            {
+                return this.Document.TextField(Find.ById(ConfirmPasswordId)).Text;
+            }
+            set
+            {
+                Document.TextField(Find.ById(ConfirmPasswordId)).TypeText(value);
+            }
+        }
+        public string PasswordStrength
+        {
+            get
+            {
+                return this.Document.Div(Find.ById(PasswordStrengthId)).InnerHtml;
+            }
+        }
+        public string ValidationErrorListText
+        {
+            get
+            {
+                return this.Document.List(Find.ById(ValidationErrorListId)).Text;
+            }
+        }
+
+        public void ClickRegisterButton()
+        {
+            Document.Button(Find.ById(DoRegisterId)).Click();
+        }
+    }
+}
